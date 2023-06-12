bae290b866f44b26cf658b2383b0e130efc1f2e2
guillermooom <97970403+guillermooom@users.noreply.github.com>
2023-06-13 01:39:39 +0200

    Add files via upload

---
+++ config/config-pendientes.php
@@ -0,0 +1,15 @@
+<?php
+function listado_pendientes($conn){
+    $stmt = $conn->prepare("SELECT titulo,descrip FROM publi");
+        
+    $stmt->execute();
+
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $result = $stmt->fetchAll();
+
+    foreach($result as $dat){
+        echo '<h3>'. $dat["titulo"].'</h3><br>';
+        echo '<p>'.$dat["descrip"].'</p>';
+    }
+}
+?>
